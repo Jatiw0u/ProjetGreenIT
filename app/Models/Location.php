d703575c9597235b6dbc50544cfbf022cd14348b
Jatiw0u <maxime.ducornet@etud.u-picardie.fr>
2024-06-03 20:37:16 +0200

    Configuration_Fusion_Du_Fichier_Location

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -1,49 +1,33 @@
 <?php
-<<<<<<< HEAD
 
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-=======
-namespace App\Models;
-
->>>>>>> eed4e944ab0716d525b6f023761b73a1853fa534
 use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-<<<<<<< HEAD
     use HasFactory;
 
     protected $table = 'location';
 
-    // Définition des éléments de la table
     protected $fillable = [
+        'idLocation',
         'NameLocation',
         'Country',
         'longitude',
         'latitude',
     ];
 
-    // Si les timestamps ne sont pas utilisés, vous pouvez les désactiver
     public $timestamps = false;
 
     public function carboneIntensities()
     {
-        return $this->hasMany(carbone_intensity::class, 'IdLocation', 'IdLocation');
+        return $this->hasMany(CarboneIntensity::class, 'IdLocation', 'IdLocation');
     }
 
+    public function electricalDemands()
+    {
+        return $this->hasMany(Electrical_Demand::class, 'Id_Location', 'IdLocation');
+    }
 }
-=======
-    protected $table = 'location'; // Nom de la table dans la base de données
-
-    // Définir les attributs de la table
-    protected $fillable = [
-        'idLocation',
-        'NameLocation',
-        'Country',
-        'Longitude',
-        'Latitude',
-    ];
-}
->>>>>>> eed4e944ab0716d525b6f023761b73a1853fa534
